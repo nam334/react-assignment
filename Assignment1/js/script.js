e0f2f6a5de1ee1c25d5bfa5936e89326101f08e3
nam334 <namratadas334@gmail.com>
2022-11-17 19:04:53 +0530

    Commit for react assignment 1

---
+++ Assignment1/js/script.js
@@ -0,0 +1,5 @@
+const rootDiv = document.createElement("div");
+const heading = document.createElement("h1");
+heading.innerHTML='Namaste React!'
+rootDiv.appendChild(heading)
+document.body.appendChild(rootDiv)
